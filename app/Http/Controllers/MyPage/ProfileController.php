0c052df2e7cf6b0689a2dc7680ccf3f88097485e
masakikona <makisankonan@gmail.com>
2021-07-15 17:09:45 +0900

    add/profile_avater_set

--- app/Http/Controllers/MyPage/ProfileController.php
+++ app/Http/Controllers/MyPage/ProfileController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\MyPage;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Mypage\Profile\EditRequest;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
@@ -12,5 +17,53 @@ class ProfileController extends Controller
      {
          return view('mypage.profile_edit_form')
              ->with('user', Auth::user());
+     }
+
+     public function editProfile(EditRequest $request)
+     {
+         $user = Auth::user();
+ 
+         $user->name = $request->input('name');
+
+         if ($request->has('avatar')) {
+            $fileName = $this->saveAvatar($request->file('avatar'));
+            $user->avatar_file_name = $fileName;
+        }
+
+
+         $user->save();
+ 
+         return redirect()->back()
+             ->with('status', 'プロフィールを変更しました。');
+     }
+
+     /**
+      * アバター画像をリサイズして保存します
+      *
+      * @param UploadedFile $file アップロードされたアバター画像
+      * @return string ファイル名
+      */
+      private function saveAvatar(UploadedFile $file): string
+      {
+          $tempPath = $this->makeTempPath();
+  
+          Image::make($file)->fit(200, 200)->save($tempPath);
+  
+          $filePath = Storage::disk('public')
+              ->putFile('avatars', new File($tempPath));
+  
+          return basename($filePath);
+      }
+
+       /**
+      * 一時的なファイルを生成してパスを返します。
+      *
+      * @return string ファイルパス
+      */
+     private function makeTempPath(): string
+     {
+         $tmp_fp = tmpfile();
+         $meta   = stream_get_meta_data($tmp_fp);
+         return $meta["uri"];
      }
 }
