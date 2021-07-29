55197200d4bffaa6e85d2d693817a0c724f0ace2
masakikona <makisankonan@gmail.com>
2021-07-30 06:42:03 +0900

    likefunction/complete

--- public/js/app.js
+++ public/js/app.js
@@ -1989,15 +1989,6 @@ __webpack_require__.r(__webpack_exports__);
 // import { faSearch, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faCamera } from '@fortawesome/free-solid-svg-icons'
 // library.add(faSearch, faAddressCard, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faClock, faCamera);
 // dom.watch();
-// document.querySelector('.image-picker input')
-//   .addEventListener('change', (e) => {
-//     const input = e.target;
-//     const reader = new FileReader();
-//     reader.onload = (e) => {
-//       input.closest('.image-picker').querySelector('img').src = e.target.result
-//     };
-//     reader.readAsDataURL(input.files[0]);
-//   });
 
 
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,15 +5,7 @@
 
 // library.add(faSearch, faAddressCard, faStoreAlt, faShoppingBag, faSignOutAlt, faYenSign, faClock, faCamera);
 // dom.watch();
-// document.querySelector('.image-picker input')
-//   .addEventListener('change', (e) => {
-//     const input = e.target;
-//     const reader = new FileReader();
-//     reader.onload = (e) => {
-//       input.closest('.image-picker').querySelector('img').src = e.target.result
-//     };
-//     reader.readAsDataURL(input.files[0]);
-//   });
+
 
 import './bootstrap'
 import Vue from 'vue'
