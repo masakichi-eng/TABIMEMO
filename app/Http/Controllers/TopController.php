5e383bbc75199962ca687cbdce940418e0b57cd5
masakikona <makisankonan@gmail.com>
2021-08-27 11:27:58 +0900

    add_top_page

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
+use App\Models\PrimaryCategory;
+
+class TopController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all()->sortByDesc('created_at')
+            ->load(['user', 'likes', 'tags']);
+
+        $query = Item::query();
+
+        // PostgreSQLの場合
+        $items = $query->orderByRaw("(CASE state WHEN '1' THEN 'selling'
+                                                      WHEN '2' THEN 'bought' END)")
+            ->orderBy('id', 'DESC')
+            ->paginate(3);
+
+        $categories = PrimaryCategory::query()
+            ->get();
+
+        return view('tops.index')
+            ->with('articles', $articles)
+            ->with('items', $items)
+            ->with('user', Auth::user())
+            ->with('categories', $categories);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/items/top', 'ItemsController@showItems')->name('top');
 
 Auth::routes();
 
-Route::get('/', 'ArticleController@index')->name('articles.index'); 
+Route::get('/top', 'TopController@index')->name('tops.index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
 Route::prefix('articles')->name('articles.')->group(function () {
@@ -28,26 +29,26 @@ Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 
 Route::middleware('auth')
-      ->group(function () {
-          Route::get('sell', 'SellController@showSellForm')->name('sell');
-          Route::post('sell', 'SellController@sellItem')->name('sell');
-          Route::get('items/{item}/buy', 'ItemsController@showBuyItemForm')->name('item.buy');
-          Route::post('items/{item}/buy', 'ItemsController@buyItem')->name('item.buy');
-      });
+    ->group(function () {
+        Route::get('sell', 'SellController@showSellForm')->name('sell');
+        Route::post('sell', 'SellController@sellItem')->name('sell');
+        Route::get('items/{item}/buy', 'ItemsController@showBuyItemForm')->name('item.buy');
+        Route::post('items/{item}/buy', 'ItemsController@buyItem')->name('item.buy');
+    });
 
 Route::prefix('mypage')
-     ->namespace('MyPage')
-     ->group(function () {
-         Route::get('/{name}/likes', 'ProfileController@likes')->name('mypage.likes');
-         Route::middleware('auth')->group(function () {
-             Route::get('edit-profile', 'ProfileController@showProfileEditForm')->name('mypage.edit-profile');
-             Route::post('edit-profile', 'ProfileController@editProfile')->name('mypage.edit-profile');
-             Route::get('sold-items', 'SoldItemsController@showSoldItems')->name('mypage.sold-items');
-             Route::get('bought-items', 'BoughtItemsController@showBoughtItems')->name('mypage.bought-items');
-             Route::get('/{name}', 'ProfileController@show')->name('mypage.show');
-             Route::put('/{name}/follow', 'ProfileController@follow')->name('follow');
-             Route::delete('/{name}/follow', 'ProfileController@unfollow')->name('unfollow');
-             Route::get('/{name}/followings', 'ProfileController@followings')->name('mypage.followings');
-             Route::get('/{name}/followers', 'ProfileController@followers')->name('mypage.followers');
+    ->namespace('MyPage')
+    ->group(function () {
+        Route::get('/{name}/likes', 'ProfileController@likes')->name('mypage.likes');
+        Route::middleware('auth')->group(function () {
+            Route::get('edit-profile', 'ProfileController@showProfileEditForm')->name('mypage.edit-profile');
+            Route::post('edit-profile', 'ProfileController@editProfile')->name('mypage.edit-profile');
+            Route::get('sold-items', 'SoldItemsController@showSoldItems')->name('mypage.sold-items');
+            Route::get('bought-items', 'BoughtItemsController@showBoughtItems')->name('mypage.bought-items');
+            Route::get('/{name}', 'ProfileController@show')->name('mypage.show');
+            Route::put('/{name}/follow', 'ProfileController@follow')->name('follow');
+            Route::delete('/{name}/follow', 'ProfileController@unfollow')->name('unfollow');
+            Route::get('/{name}/followings', 'ProfileController@followings')->name('mypage.followings');
+            Route::get('/{name}/followers', 'ProfileController@followers')->name('mypage.followers');
+        });
     });
-});
